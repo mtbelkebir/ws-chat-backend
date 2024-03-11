2f4ec23fbec7f48e1f7bec57c071a0d9691f5b6f
mtbelkebir <mohandtahar.belkebir@se.univ-bejaia.dz>
2024-03-11 22:16:54 +0100

    Create user model

---
+++ src/main/java/dz/mtbelkebir/wschat/api/model/UserRepository.java
@@ -0,0 +1,10 @@
+package dz.mtbelkebir.wschat.api.model;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> getByUsername(String username);
+}
