d16425248323eadf3289b60f44ba765b0b31c801
mtbelkebir <mohandtahar.belkebir@se.univ-bejaia.dz>
2024-03-14 17:12:37 +0100

    Configure auth manager

--- src/main/java/dz/mtbelkebir/wschat/api/config/ApplicationConfig.java
+++ src/main/java/dz/mtbelkebir/wschat/api/config/ApplicationConfig.java
@@ -2,8 +2,14 @@ package dz.mtbelkebir.wschat.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import dz.mtbelkebir.wschat.api.model.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +23,24 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService() {
         return username -> userRepository.getByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(getPasswordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new Argon2PasswordEncoder(2, 16, 1, 16, 2);
     }
 
 }
