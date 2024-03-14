7701e1773a28f58d3cf4f65cb76c3816a5ba066a
mtbelkebir <mohandtahar.belkebir@se.univ-bejaia.dz>
2024-03-15 00:29:05 +0100

    Password alg is now BCrypt

--- src/main/java/dz/mtbelkebir/wschat/api/config/ApplicationConfig.java
+++ src/main/java/dz/mtbelkebir/wschat/api/config/ApplicationConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import dz.mtbelkebir.wschat.api.model.UserRepository;
@@ -40,7 +40,7 @@ public class ApplicationConfig {
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return new Argon2PasswordEncoder(2, 16, 1, 16, 2);
+        return new BCryptPasswordEncoder();
     }
 
 }
