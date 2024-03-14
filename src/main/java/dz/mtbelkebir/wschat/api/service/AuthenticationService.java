a8858ed07dab085e11e035b6bba239b9e52c6d39
mtbelkebir <mohandtahar.belkebir@se.univ-bejaia.dz>
2024-03-15 00:30:21 +0100

    Remove unused import

--- src/main/java/dz/mtbelkebir/wschat/api/service/AuthenticationService.java
+++ src/main/java/dz/mtbelkebir/wschat/api/service/AuthenticationService.java
@@ -1,6 +1,5 @@
 package dz.mtbelkebir.wschat.api.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
